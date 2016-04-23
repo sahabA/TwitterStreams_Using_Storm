1fff5048592f52a4e5c45cd835f0edf17be2464b
sahabA <sahab@ischool.berkeley.edu>
2016-04-22 20:04:56 -0400

    Update histogram.py
    Error checking for bad args as well as allowing any order for the two numbers

--- Ex2/histogram.py
+++ Ex2/histogram.py
@@ -1,13 +1,29 @@
 import psycopg2
 import sys
 
+
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 
 cur = conn.cursor()
 
 #print sys.argv[2]
-cur.execute("SELECT word, count from Tweetwordcount  Where count between %s AND %s order by count desc", (sys.argv[1],sys.argv[2]))
-records = cur.fetchall()
+try:
+ a = int(sys.argv[1])
 
-for rec in records:
+ b = int(sys.argv[2])
+
+ if(b<a):
+  c=b
+  d=a
+ else:
+  c=a
+  d=b
+
+ cur.execute("SELECT word, count from Tweetwordcount  Where count between %s AND %s order by count desc", (c,d))
+ records = cur.fetchall()
+
+ for rec in records:
                 print  rec[0], ": ", rec[1]
+
+except Exception as error:
+ print("Please provide two numbers separated by space)")
